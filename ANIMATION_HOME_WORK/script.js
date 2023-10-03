6ec0269b5ea685927826934d240dc58e3f7752e8
ArturLiu <liuart07@gmail.com>
2023-10-03 15:13:21 +0600

    first commit

---
+++ ANIMATION_HOME_WORK/script.js
@@ -0,0 +1,6 @@
+const animatedDiv = document.getElementById('animation');
+
+animation.addEventListener('click', () => {
+    animation.classList.toggle('move');
+    animation.classList.toggle('resize');
+});
