784180f2ac99e8c06fe14cbc7d95a6138a151508
ArturLiu <liuart07@gmail.com>
2023-10-17 15:48:32 +0600

    first commit

---
+++ ARRAY_AND_OBJECTS_HOME_WORK/main.js
@@ -0,0 +1,89 @@
+//! 1
+
+// const person = {
+//     name: 'Arthur',
+//     age: 16,
+//     city: 'Bishkek'
+// }
+
+//! 2
+
+// person.age = 20
+
+// console.log(preson)
+
+//! 3
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// const length = numbers.length
+
+// console.log(`Длинна массива: ${length}`)
+
+//! 4
+
+// const animals = []
+
+// animals.push("Динозавр")
+// animals.push("Собака")
+// animals.push("Кот")
+// animals.push("Слон")
+// animals.push("Крокодил")
+
+// console.log(animals)
+
+//! 5
+
+// const one = {
+//     name: 'Arthur',
+//     age: 16,
+//     city: 'Bishkek'
+// }
+
+// const two = {
+//     name: 'Timur',
+//     age: 14,
+//     city: 'Moscow'
+// }
+
+// const three = {
+//     name: 'Emka',
+//     age: 17,
+//     city: 'Istanbul'
+// }
+// const people = [one, two, three]
+
+// console.log(people)
+
+//! 6
+
+// const data = [7, "Hello world!", { name: "Arthur", age: 16 }]
+
+// console.log(data)
+
+//! 7
+
+// const fruits = ["orange", "banana", "watermelon", "pearl", "cherry"]
+
+// console.log(fruits[2])
+
+//! 8
+
+// const book = {
+//     title: "Капитан немо",
+//     author: "Жуль верн",
+//     year: 1802
+// };
+
+// console.log(book.author)
+
+
+
+
+
+
+
+
+
+
+
