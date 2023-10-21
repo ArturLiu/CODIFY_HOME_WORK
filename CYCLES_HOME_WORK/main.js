14b2a94c388d9c9cdaf342cb92dd43096e26735f
ArturLiu <liuart07@gmail.com>
2023-10-21 16:37:36 +0600

    first commit

---
+++ CYCLES_HOME_WORK/main.js
@@ -0,0 +1,90 @@
+//! 1
+
+// for (let number = 2; number <= 50; number++) {
+//     let result = true
+//     for (let i = 2; i < number; i++) {
+//         if (number % i === 0) {
+//             result = false
+//             break
+//         }
+//     }
+//     if (result) {
+//         console.log(number)
+//     }
+// }
+
+//! 2
+
+// const N = prompt("Введите число")
+
+// if (!isNaN(N)) {
+//     for (let i = 1; i <= N; i++) {
+//         if (i % 3 === 0 || i % 5 === 0) {
+//             console.log(i)
+//         }
+//     }
+// } 
+
+//! 3
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+//! 4
+
+// const input = prompt('')
+// const num = input * 1
+
+// let sum = 0
+// z
+// if (!isNaN(num)) {
+//     for (let i = 1; i <= num; i++) {
+//         sum += i
+//     }
+//     console.log(sum)
+// }
+
+//! 5
+
+// for (let i = 1; i <= 10; i++) {
+//     result = 7 * i
+//     console.log(result)
+// }
+
+//! 6
+
+// const input = prompt('')
+// const num = input + 1
+
+// let sum = 0
+
+// if (!isNaN(num)) {
+//     for (let i = 1; i <= num; i++) {
+//         sum += i
+//     }
+//     console.log(sum)
+// }
+
+//! 7 
+
+// const height = prompt('')
+
+// if (!isNaN(height)) {
+//     for (let i = 1; i <= height; i++) {
+//         let stage = ''
+//         for (let j = 1; j <= height - i; j++) {
+//             stage += ' '
+//         }
+//         for (let k = 1; k <= 2 * i - 1; k++) {
+//             stage += '*'
+//         }
+//         console.log(stage);
+//     }
+// }
+
+
+
+
+
+
