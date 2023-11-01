b8331104f701a609bb21e1b3632da87390976d27
ArturLiu <liuart07@gmail.com>
2023-11-01 22:00:02 +0600

    first commit

---
+++ FUNCTIONS_AND_METHODS_HOME_WORK/main.js
@@ -0,0 +1,202 @@
+//! 1 Задание для строк
+
+// function concatStrings(txt1, txt2) {
+//     const result = txt1.concat(txt2)
+//     return result
+//   }
+  
+//   const text1 = "Hello "
+//   const text2 = "World"
+//   const text1_2 = concatStrings(text1, text2)
+//   console.log(text1_2)
+
+//! 2
+
+// function changeWord(text, underText, replacement) {
+//   return text.replaceAll(text, replacement);
+// }
+
+// const text1 = "Hello world";
+// const changedText = changeWords(text1, "Hello", "Hi")
+// console.log(changedText)
+
+//! 3
+
+// function textToLowerCase(text) {
+//   return text.toLowerCase()
+// }
+
+// const text1 = "HELLO WORLD"
+// const lowerCaseText = textToLowerCase(text1)
+// console.log(lowerCaseText)
+
+//! 4
+
+// function checkText(text, underText) {
+//   return text.includes(underText)
+// }
+
+// const text1 = "Hello world"
+// const textToCheck = "world"
+// const haveText = checkText(text, textToCheck)
+// console.log(haveText)
+
+//! 5
+
+// const getIndexOf = (text, underText) => text.indexOf(underText)
+
+// const text = "Hello world"
+// const underTextToFind = "world"
+// const index = getIndexOf(text, underTextToFind)
+// console.log(index)
+
+//! 6
+
+// function splitString(text) {
+//   return text.split(" ")
+// }
+
+// const text1 = "one two three four five"
+// const arr = splitString(text1)
+// console.log(arr)
+
+//! 7
+
+// function sliceText(text, startIndex, endIndex) {
+//   return text.slice(startIndex, endIndex)
+// }
+
+// const text = "one two three four five"
+// const startIndex = 4
+// const endIndex = 10
+// const underText = sliceText(text, startIndex, endIndex)
+// console.log(underText)
+
+//! 8 Задание для чисел
+
+// function convertToString(num) {
+//   return num.toString();
+// }
+
+// const num1 = 42
+// const stringValue = convertToString(numericValue)
+// console.log(stringValue)
+
+//! 9
+
+// function convertToFloat(text) {
+//   return parseFloat(text)
+// }
+
+// const num1 = "7.7235"
+// const floatValue = convertToFloat(num1)
+// console.log(floatValue)
+
+//! 10
+
+// const sumNumbers = (num1, num2) => num1 + num2
+
+// const result = sumNumbers(1, 1)
+// console.log(result)
+
+//! 11
+
+// function getFixedNumber(number) {
+//   const changedNum = number.toFixed(2)
+//   return changedNum
+// }
+
+// const numericValue = 6.7675446
+// const changedText = getFixedNumber(numericValue)
+// console.log(changedText)
+
+//! 12
+
+// function parseIntFromString(text) {
+//   return parseInt(text, 10)
+// }
+
+// const num1 = "42";
+// const parseIntresult = parseIntFromString(num1)
+// console.log(parseIntresult)
+
+//! 13
+
+// function addNumbers(num1, num2) {
+//   return num1 + num2
+// }
+
+// const result = addNumbers(1, 1)
+// console.log(result)
+
+//! 14
+
+// function parseFloatFromString(text) {
+//   return parseFloat(text);
+// }
+
+// const text1 = "4.5235";
+// const floatValue = parseFloatFromString(floatString);
+// console.log(floatValue)
+
+//! 15 Задание для массивов
+
+// const fruits = ["apple", "banana", "cherry"]
+// fruits.push("orange")
+// fruits.pop()
+// console.log(fruits)
+
+//! 16
+
+// const numbers = [1, 2, 3, 4, 5]
+// const evenNumbers = numbers.filter(num => num % 2 === 0)
+// const evenNumbersString = evenNumbers.join(",")
+// console.log(evenNumbersString)
+
+//! 17
+
+// const colors = ["red", "blue", "green"]
+// colors.unshift("yellow")
+// colors.shift()
+// console.log(colors)
+
+//! 18
+
+// const firstArray = [1, 2, 3]
+// const secondArray = [4, 5, 6]
+// const mergedArray = firstArray.concat(secondArray)
+// const hasValue = mergedArray.includes(5)
+// console.log(hasValue)
+
+//! 19
+
+// const animals = ["cat", "dog", "elephant"]
+// animals.push("lion")
+// const index = animals.indexOf("elephant")
+// console.log(index)
+
+//! 20
+
+// const numbers = [7, 8, 9, 10]
+// const slicedAndReversed = numbers.slice(0, 2).reverse()
+// const resultString = slicedAndReversed.toString()
+// console.log(resultString)
+
+//! 21
+
+// const fruits = ["apple", "banana", "cherry"]
+// fruits.unshift("pear")
+// fruits.shift()
+// console.log(fruits)
+
+//! 22
+
+// const firstArray = [1, 2, 3]
+// const secondArray = [4, 5, 6]
+// const mergedArray = firstArray.concat(secondArray)
+// const mergedArrayString = mergedArray.join(",")
+// console.log(mergedArrayString)
+
+
+
+
